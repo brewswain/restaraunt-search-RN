41856eb53e97fdf5bcc422f52923acf84a7214d7
Brandon Lee <brandonlee2543@hotmail.com>
2020-12-16 17:06:34 -0400

    Set up navigator

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { SearchScreen } from "./src/screens";
 
-export default function App() {
-  // Left in to remind me that i have .env configured
-  //
-  // console.log(process.env.CLIENT_ID);
-  // console.log(process.env.API_KEY);
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Left in to remind me that i have .env configured
+//
+// console.log(process.env.CLIENT_ID);
+// console.log(process.env.API_KEY);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
   },
-});
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Restaurant Search",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
