5b2a4ff26b55a9c38bee76391c25e20c025664dd
Brandon Lee <brandonlee2543@hotmail.com>
2020-12-16 16:28:25 -0400

    configured to show .env potential

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  // Left in to remind me that i have .env configured
+  // console.log(process.env.CLIENT_ID);
+  // console.log(process.env.API_KEY);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
