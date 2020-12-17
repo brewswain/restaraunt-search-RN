10f8a589e9b2b29fe45fcb3dc5a6de8bc56da608
Brandon Lee <brandonlee2543@hotmail.com>
2020-12-17 18:53:39 -0400

    Implemented basic API call using axios

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,22 +2,37 @@ import React, { useState } from "react";
 
 import { StyleSheet, Text, View } from "react-native";
 import { SearchBar } from "../components";
+import yelp from "../api/yelp";
 
 const SearchScreen = (props) => {
+  const [term, setTerm] = useState("");
+  const [businesses, setBusinesses] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
   const { navigate } = props.navigation;
   const {} = styles;
-  const [term, setTerm] = useState("");
+
+  const searchApi = async () => {
+    try {
+      const res = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "san jose",
+        },
+      });
+
+      setBusinesses(res.data.businesses);
+    } catch (error) {
+      setErrorMessage("Something went wrong");
+    }
+  };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => {
-          setTerm(newTerm);
-        }}
-        onTermSubmit={() => console.log("Term was submitted")}
-      />
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {businesses.length} results</Text>
     </View>
   );
 };
